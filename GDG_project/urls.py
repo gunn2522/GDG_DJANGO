e8de89f5402e2164ae37d95b676003c43fed13e7
Gunn Malhotra <166500066+gunn2522@users.noreply.github.com>
2025-07-15 12:18:57 +0530

    Initial commit

--- GDG_project/urls.py
+++ GDG_project/urls.py
@@ -1,30 +1,16 @@
-"""
-URL configuration for GDG_project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
+from django.urls import path, include
 from django.shortcuts import redirect
-from django.urls import path,include
 
-from GDG_project import settings
-from dashboard.views import dashboard_view
-from django.conf.urls.static import static
+def home_redirect(request):
+    if request.user.is_authenticated:
+        return redirect('dashboard')  # Redirect to dashboard if logged in
+    return redirect('login')  # Redirect to login if not logged in
+
 urlpatterns = [
+    path('', home_redirect, name='home'),  # 👈 Add this to handle `/`
     path('admin/', admin.site.urls),
-    path('dashboard/', dashboard_view, name='dashboard'),
-    path('', lambda request: redirect('login')),  # 👈 Redirect to login page
+    path('dashboard/', include('dashboard.urls')),
     path('users/', include('users.urls')),
     path('events/', include('events.urls')),
     path('attendence/', include('attendence.urls')),
@@ -32,6 +18,4 @@ urlpatterns = [
     path('infra_issues/', include('infra_issues.urls')),
     path('performance/', include('performance.urls')),
     path('ai_assistant/', include('ai_assistant.urls')),
-
-
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
