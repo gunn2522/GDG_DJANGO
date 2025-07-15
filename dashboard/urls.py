c08cd384d1ab83308262ad2c6d262e33854adb3f
Gunn Malhotra <166500066+gunn2522@users.noreply.github.com>
2025-07-15 12:32:51 +0530

    Initial commit

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import dashboard_view, approval_dashboard_view
 
 urlpatterns = [
-    path('/dashboard_view/', dashboard_view, name='dashboard'),
+    path('', dashboard_view, name='dashboard'),  # ⬅ this becomes /dashboard/
 
     path("dashboard/approval_dashboard/", approval_dashboard_view, name="approval_dashboard"),
 
