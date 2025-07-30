c5aa60402c8b8f7e9c61681283cb161f7dd370fb
Gunn Malhotra <166500066+gunn2522@users.noreply.github.com>
2025-07-30 17:36:17 +0530

    Fix Vercel Django deployment

--- api/index.py
+++ api/index.py
@@ -1,13 +1,16 @@
-# api/index.py
-
 import os
 import sys
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-sys.path.append('./GDG_project')
+from django.core.wsgi import get_wsgi_application
 
+# Add your project root to the sys path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+# Set the settings module
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "GDG_project.settings")
+
+# Allow unsafe async for serverless
 os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
 
-from django.core.wsgi import get_wsgi_application
+# Export the app that Vercel looks for
 app = get_wsgi_application()
