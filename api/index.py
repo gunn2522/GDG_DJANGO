93934832827f14bd092853cbdb23a55a47560cc8
Gunn Malhotra <166500066+gunn2522@users.noreply.github.com>
2025-07-30 17:30:04 +0530

    Fix: use correct WSGI entry point for Vercel

--- api/index.py
+++ api/index.py
@@ -3,14 +3,11 @@
 import os
 import sys
 
-# Add your Django project directory to the Python path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.append('./GDG_project')
 
-# Set Django settings
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "GDG_project.settings")
 os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
 
-# Import and export the WSGI application
 from django.core.wsgi import get_wsgi_application
 app = get_wsgi_application()
